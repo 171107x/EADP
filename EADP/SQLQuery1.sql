377b7798b908339cde78153c020e26f501ec0876
171107x <171107x@mymail.nyp.edu.sg>
2018-11-29 18:04:44 +0800

    .

---
+++ EADP/SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Student] (
+  [Student_Admin] VARCHAR (12) NOT NULL PRIMARY KEY,
+  [Student_name] VARCHAR (60) NOT NULL,
+  [Email] VARCHAR (60) NOT NULL,
+  [Password] BINARY(64) NOT NULL,
+  [School] VARCHAR (12) NOT NULL,
+  [Gender] char(1) NOT NULL,
+  [DOB] DATE NOT NULL,
+  [PEM Group] VARCHAR (12) NOT NULL,
+
+);
+
+
