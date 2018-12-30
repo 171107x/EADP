ced5f24da1ecd2be5cb7a2f929f444baeb3aeee8
Ho Joey <170852T@mymail.nyp.edu.sg>
2018-12-30 20:15:45 +0800

    Trip Management Added
    All the pages and functionality of Trip management that has been completed thus far(first push).

---
+++ EADP/EADP/DAL/tripDAO.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace EADP.DAL
+{
+    public class tripDAO
+    {
+        string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+
+        /*       public trip getTripById(int TripID)
+               {
+                   SqlDataAdapter da;
+                   DataSet ds = new DataSet();
+
+                   StringBuilder sqlCommand = new StringBuilder();
+                   sqlCommand.AppendLine("Select * from Trip");
+                   //sqlCommand.AppendLine("where TripID = 1");
+
+                   trip obj = new trip();
+
+                   SqlConnection myConn = new SqlConnection(DBConnect);
+                   da = new SqlDataAdapter(sqlCommand.ToString(), myConn);
+                  // da.SelectCommand.Parameters.AddWithValue("1", TripID);
+
+                   da.Fill(ds, "tripTable");
+                   int rec_cnt = ds.Tables["tripTable"].Rows.Count;
+                   if (rec_cnt > 0)
+                   {
+                       DataRow row = ds.Tables["tripTable"].Rows[0];
+                       obj.Country = row["Country"].ToString();
+                       obj.Description = row["Description"].ToString();
+                   }
+                   else
+                   {
+                       obj = null;
+                   }
+
+                   return obj;
+               }
+               */
+        public List<trip> getTripInfo()
+        {
+            List<trip> tdList = new List<trip>();
+            DataSet ds = new DataSet();
+            DataTable tdData = new DataTable();
+
+            StringBuilder sqlStr = new StringBuilder();
+            sqlStr.AppendLine("SELECT * From trip");
+            //sqlStr.AppendLine("where TripID = 1");
+
+            SqlConnection myConn = new SqlConnection(DBConnect);
+            SqlDataAdapter da = new SqlDataAdapter(sqlStr.ToString(), myConn);
+
+
+            //da.SelectCommand.Parameters.AddWithValue("1", TripID);
+            da.Fill(ds, "TableInfo");
+
+            int rec_cnt = ds.Tables["TableInfo"].Rows.Count;
+            if (rec_cnt > 0)
+            {
+                foreach (DataRow row in ds.Tables["TableInfo"].Rows)
+                {
+                    trip myTD = new trip();
+
+                    myTD.TripID = Convert.ToString(row["TripID"]);
+                    myTD.Country = row["Country"].ToString();
+                    myTD.StartDate = Convert.ToDateTime(row["StartDate"]);
+                    myTD.EndDate = Convert.ToDateTime(row["EndDate"]);
+                    myTD.ETripPrice = Convert.ToDouble(row["ETripPrice"]);
+
+                    tdList.Add(myTD);
+                }
+            }
+            else
+            {
+                tdList = null;
+            }
+
+            return tdList;
+        }
+
+        public int InsertTrip(string TripID, DateTime StartDate, DateTime EndDate, string Country, double ETripPrice)
+        {
+
+            StringBuilder sqlStr = new StringBuilder();
+            int result = 0;
+            SqlCommand sqlCmd = new SqlCommand();
+
+            sqlStr.AppendLine("INSERT INTO Trip (TripID, StartDate, EndDate, Country, ETripPrice)");
+            sqlStr.AppendLine("VALUES (@paraTripID, @paraStartDate, @paraEndDate, @paraCountry, @paraPrice)");
+
+            SqlConnection myConn = new SqlConnection(DBConnect);
+
+            sqlCmd = new SqlCommand(sqlStr.ToString(), myConn);
+
+            sqlCmd.Parameters.AddWithValue("@paraTripID", TripID);
+            sqlCmd.Parameters.AddWithValue("@paraStartDate", StartDate);
+            sqlCmd.Parameters.AddWithValue("@paraEndDate", EndDate);
+            sqlCmd.Parameters.AddWithValue("@paraCountry", Country);
+            sqlCmd.Parameters.AddWithValue("@paraPrice", ETripPrice);
+
+            myConn.Open();
+            result = sqlCmd.ExecuteNonQuery();
+
+            myConn.Close();
+
+            return result;
+
+        }
+    }
+}
---
+++ EADP/EADP/TripManagement.aspx.cs
@@ -0,0 +1,42 @@
+﻿using EADP.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EADP
+{
+    public partial class TripManament : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            trip tripOjb = new trip();
+            tripDAO tripDAO = new tripDAO();
+            //tripOjb = tripDAO.getTripById(1);
+            //Lbl_country.Text = tripOjb.Country;
+            //Lbl_description.Text = tripOjb.Description;
+
+
+            List<trip> tdList = new List<trip>();
+            tdList = tripDAO.getTripInfo();
+            GridViewTrip.DataSource = tdList;
+            GridViewTrip.DataBind();
+        }
+
+        protected void BtnConfirm_Click(object sender, EventArgs e)
+        {
+            string TripID = Convert.ToString(tbTripID.Text.ToString());
+            DateTime StartDate = Convert.ToDateTime(tbStartDate.Text.ToString());
+            DateTime EndDate = Convert.ToDateTime(tbEndDate.Text.ToString());
+            string Country = tbCountry.Text.ToString();
+            double ETripPrice = Convert.ToDouble(tbETripPrice.Text.ToString());
+
+
+            tripDAO newTrip = new tripDAO();
+            newTrip.InsertTrip(TripID, StartDate, EndDate, Country, ETripPrice);
+            Response.Redirect("TripManagement.aspx");
+        }
+    }
+}
