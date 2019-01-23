209409065d714b6b69a2d38b27ca1169b25f869a
J-I-M-S <32917926+J-I-M-S@users.noreply.github.com>
2019-01-23 20:56:54 +0800

    Country Database
    Country Database

---
+++ EADP/EADP/DAL/country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EADP.DAL
+{
+    public class country
+    {
+        public int countryID { get; set; }
+        public string countryName { get; set; }
+    }
+}
---
+++ EADP/EADP/DAL/countryDAO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace EADP.DAL
+{
+    public class countryDAO
+    {
+        string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+        public int insertCountry(string countryName)
+        {
+            StringBuilder sqlStr = new StringBuilder();
+            int result = 0;    // Execute NonQuery return an integer value
+            SqlCommand sqlCmd = new SqlCommand();
+
+            sqlStr.AppendLine("INSERT INTO Country (countryName)");
+            sqlStr.AppendLine("VALUES (@paraCountryName)");
+
+            SqlConnection myConn = new SqlConnection(DBConnect);
+
+            sqlCmd = new SqlCommand(sqlStr.ToString(), myConn);
+
+            sqlCmd.Parameters.AddWithValue("@paraCountryName", countryName);
+
+            myConn.Open();
+            result = sqlCmd.ExecuteNonQuery();
+
+            myConn.Close();
+
+            return result;
+        }
+    }
+}
