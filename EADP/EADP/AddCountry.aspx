fb132c17c2fd3d56cf7c4cce1cf60fec57813abe
J-I-M-S <32917926+J-I-M-S@users.noreply.github.com>
2019-01-27 16:19:00 +0800

    AddCountry
    addcountry to database

---
+++ EADP/EADP/AddCountry.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteMaster.Master" AutoEventWireup="true" CodeFile="AddCountry.aspx.cs" Inherits="EADP.AddCountry" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <br />
+    <br />
+    <br />
+    <div class="container">
+        <asp:Label ID="lblCountry" runat="server" Text="Country:"></asp:Label>
+        <asp:TextBox ID="textboxCountry" runat="server" class="form-control"></asp:TextBox>
+        <asp:Button ID="btnInsert" runat="server" Text="Insert" class="btn btn-primary" OnClick="btnInsert_Click" />
+        </div>
+</asp:Content>
---
+++ EADP/EADP/AddCountry.aspx.cs
@@ -0,0 +1,56 @@
+﻿using EADP.DAL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EADP
+{
+    public partial class AddCountry : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection myConn = new SqlConnection(DBConnect);
+            DataTable dtStaff = new DataTable();
+
+            StringBuilder sqlStr = new StringBuilder();
+            sqlStr.AppendLine("SELECT countryName, countryID FROM Country");
+
+            SqlDataAdapter da = new SqlDataAdapter(sqlStr.ToString(), myConn);
+            da.Fill(dtStaff);
+
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e,string countryName)
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+
+            StringBuilder sqlStr = new StringBuilder();
+            int result = 0;    // Execute NonQuery return an integer value
+            SqlCommand sqlCmd = new SqlCommand();
+
+            sqlStr.AppendLine("INSERT INTO Country (countryName)");
+            sqlStr.AppendLine("VALUES (@paraCountryName)");
+
+            SqlConnection myConn = new SqlConnection(DBConnect);
+
+            sqlCmd = new SqlCommand(sqlStr.ToString(), myConn);
+
+            sqlCmd.Parameters.AddWithValue("@paraCountryName", countryName);
+
+            myConn.Open();
+            result = sqlCmd.ExecuteNonQuery();
+
+            myConn.Close();
+
+            Response.Redirect("AddCountry.aspx");
+        }
+    }
+}
