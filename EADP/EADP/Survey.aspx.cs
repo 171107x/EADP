50bd5ff02fbbed4022046af8732556a24121f2c9
Max <32918632+SliceofButter@users.noreply.github.com>
2019-01-27 21:28:27 +0800

    TEST

--- EADP/EADP/Survey.aspx.cs
+++ EADP/EADP/Survey.aspx.cs
@@ -64,7 +64,7 @@ namespace EADP
         }
 
 
-        protected void resetBtn_Click1(object sender, EventArgs e)
+        protected void resetBtn_Click(object sender, EventArgs e)
         {
             tbQ1.Text = String.Empty;
             tbQ2.Text = String.Empty;
