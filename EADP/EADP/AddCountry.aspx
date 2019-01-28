755ff3c9956a688e10f8eb1e94aab2099e4302ec
J-I-M-S <32917926+J-I-M-S@users.noreply.github.com>
2019-01-28 11:57:00 +0800

    ,

--- EADP/EADP/AddCountry.aspx
+++ EADP/EADP/AddCountry.aspx
@@ -1,13 +1,48 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteMaster.Master" AutoEventWireup="true" CodeBehind="AddCountry.aspx.cs" Inherits="EADP.AddCountry" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteMaster.Master" AutoEventWireup="true" CodeFile="AddCountry.aspx.cs" Inherits="EADP.AddCountry" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <br />
     <br />
     <br />
+    <center><h1>Update Countries</h1></center>
+    <br />
+    <br />
     <div class="container">
-        <asp:Label ID="lblCountry" runat="server" Text="Country:"></asp:Label>
+        <asp:Label ID="lblCountry" runat="server" Text="Enter Country:"></asp:Label>
         <asp:TextBox ID="textboxCountry" runat="server" class="form-control"></asp:TextBox>
         <asp:Button ID="btnInsert" runat="server" Text="Insert" class="btn btn-primary" OnClick="btnInsert_Click"  />
+        <asp:Label ID="lberror" runat="server" Font-Size="Medium" ForeColor="Red"></asp:Label>
+        <br />
+        <asp:GridView ID="GridViewCountry" runat="server" Width="1137px" AutoGenerateColumns="False" AllowPaging="True" PageSize="5" OnPageIndexChanging="GridViewCountry_PageIndexChanging" CssClass="table table-bordered" OnSelectedIndexChanged="GridViewCountry_SelectedIndexChanged"  DataKeyNames="CountryID" OnRowCancelingEdit="GridViewCountry_RowCancelingEdit" OnRowEditing="GridViewCountry_RowEditing" OnRowUpdating="GridViewCountry_RowUpdating" OnRowDeleting="GridViewCountry_RowDeleting">
+            <Columns>
+                <asp:TemplateField HeaderText="Country ID">
+                    <ItemTemplate>
+                        <asp:Label ID="lbCountryID" runat="server" Text='<%# Eval("CountryID") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+
+                <asp:TemplateField HeaderText="Country Name">
+                    <ItemTemplate>
+                        <asp:Label ID="lbCountryName" runat="server" Text='<%# Eval("CountryName") %>'></asp:Label>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="tbCountryName" runat="server" Text='<%# Eval("CountryName") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                </asp:TemplateField>
+
+                <asp:TemplateField HeaderText="Edit Country">
+                    <ItemTemplate>
+                        <asp:Button ID="btnedit" runat="server" Text="Edit" width="80" CommandName="Edit" />
+                        </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:Button Text="Update" ID="btnupdate" runat="server" width="80" CommandName="Update" />
+                        <asp:Button Text="Cancel" ID="btncancel" runat="server" width="80" CommandName="Cancel" />
+                    </EditItemTemplate>
+                </asp:TemplateField>
+                <asp:CommandField HeaderText="Delete Country" ShowDeleteButton="true" runat="server" />
+            </Columns>
+        </asp:GridView>
         </div>
 </asp:Content>
+ 	
--- EADP/EADP/AddCountry.aspx.cs
+++ EADP/EADP/AddCountry.aspx.cs
@@ -14,42 +14,126 @@ namespace EADP
 {
     public partial class AddCountry : System.Web.UI.Page
     {
+        string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
-            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            if (!this.IsPostBack)
+            {
+                bindgrid();
+            }
+        }
+        private void bindgrid()
+        {
             SqlConnection myConn = new SqlConnection(DBConnect);
-            DataTable dtStaff = new DataTable();
+            DataTable dtCountry = new DataTable();
 
             StringBuilder sqlStr = new StringBuilder();
             sqlStr.AppendLine("SELECT countryName, countryID FROM Country");
 
             SqlDataAdapter da = new SqlDataAdapter(sqlStr.ToString(), myConn);
-            da.Fill(dtStaff);
-
+            da.Fill(dtCountry);
+            GridViewCountry.DataSource = dtCountry;
+            GridViewCountry.DataBind();
+        }
+        
+        protected void GridViewCountry_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridViewCountry.PageIndex = e.NewPageIndex;
+            bindgrid();
         }
 
+        protected void GridViewCountry_RowCommand(object sender, GridViewDeleteEventArgs e)
+        {
+            
+            
+                int index;
+                //bool bIsConverted = int.TryParse(e.CommandArgument.ToString(), out index);
+                //GridViewRow row = GridViewCountry.Rows[index];
+                
+            
+        }
 
 
         protected void btnInsert_Click(object sender, EventArgs e)
         {
+            if (textboxCountry.Text=="")
+            {
+                Response.Write("Fill in the Country Name");
+            }
+            else
+            {
+                string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+
+                StringBuilder sqlStr = new StringBuilder();
+                int result = 0;    // Execute NonQuery return an integer value
+                SqlCommand sqlCmd = new SqlCommand();
+
+                sqlStr.AppendLine("INSERT INTO Country (countryName)");
+                sqlStr.AppendLine("VALUES (@paraCountryName)");
+
+                SqlConnection myConn = new SqlConnection(DBConnect);
+
+                sqlCmd = new SqlCommand(sqlStr.ToString(), myConn);
+
+                sqlCmd.Parameters.AddWithValue("@paraCountryName", textboxCountry.Text);
+
+                myConn.Open();
+                result = sqlCmd.ExecuteNonQuery();
+
+                myConn.Close();
+
+                Response.Redirect("AddCountry.aspx");
+            }
+        }
+
+        protected void GridViewCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridViewCountry_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridViewCountry.EditIndex = e.NewEditIndex;
+            bindgrid();
+        }
+
+        protected void GridViewCountry_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridViewCountry.EditIndex = -1;
+            bindgrid();
+        }
+
+        protected void GridViewCountry_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            Label editlb = GridViewCountry.Rows[e.RowIndex].FindControl("lbCountryID") as Label;
+            TextBox edittb = GridViewCountry.Rows[e.RowIndex].FindControl("tbCountryName") as TextBox;
+
             string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
-
-            StringBuilder sqlStr = new StringBuilder();
-            int result = 0;    // Execute NonQuery return an integer value
-            SqlCommand sqlCmd = new SqlCommand();
-
-            sqlStr.AppendLine("INSERT INTO Country (countryName)");
-            sqlStr.AppendLine("VALUES (@paraCountryName)");
-
             SqlConnection myConn = new SqlConnection(DBConnect);
-
-            sqlCmd = new SqlCommand(sqlStr.ToString(), myConn);
-
-            sqlCmd.Parameters.AddWithValue("@paraCountryName", textboxCountry.Text);
-
             myConn.Open();
-            result = sqlCmd.ExecuteNonQuery();
 
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "UPDATE [Country] set CountryName=@countryname where CountryID=@countryid";
+            cmd.Parameters.AddWithValue("@countryid", editlb.Text);
+
+            cmd.Parameters.AddWithValue("@countryname", edittb.Text);
+            cmd.Connection = myConn;
+            cmd.ExecuteNonQuery();
+
+            GridViewCountry.EditIndex = -1;
+            bindgrid();
+
+        }
+
+        protected void GridViewCountry_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            SqlConnection myConn = new SqlConnection(DBConnect);
+            string id = GridViewCountry.DataKeys[e.RowIndex].Values["CountryID"].ToString();
+            int result;
+            myConn.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM Country where CountryID='"+id+"'", myConn);
+            result = cmd.ExecuteNonQuery();
             myConn.Close();
 
             Response.Redirect("AddCountry.aspx");
